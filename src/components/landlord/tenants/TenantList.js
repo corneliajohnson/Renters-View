88e5da5bcb01034cc40a52259d15ba995bbfc587
Cornelia Johnson <corneliaj91@gmail.com>
2020-10-16 09:50:38 -0400

    filtered tenants list

--- src/components/landlord/tenants/TenantList.js
+++ src/components/landlord/tenants/TenantList.js
@@ -1,18 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import { TenantContext } from "./TenantProvider";
 import { TenantCard } from "./TenantCard";
+import { parse } from "@fortawesome/fontawesome-svg-core";
 
 export const TenantList = () => {
   const { getTenants, tenants } = useContext(TenantContext);
+  const [filteredTenants, setFilteredTenants] = useState([]);
 
   useEffect(() => {
     getTenants();
   }, []);
 
+  useEffect(() => {
+    const subsetTenants = tenants.filter(
+      (tenant) =>
+        tenant.landlordId === parseInt(localStorage.landlord) && tenant.id !== 1
+    );
+
+    setFilteredTenants(subsetTenants);
+  }, [tenants]);
+
   return (
     <div className="container d-flex justify-content-around">
       <div className="row wrap m-5">
-        {tenants.map((tenant) => {
+        {filteredTenants.map((tenant) => {
           return <TenantCard key={tenant.id} tenant={tenant} />;
         })}
       </div>
