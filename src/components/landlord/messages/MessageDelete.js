35f1a29e1715d5e3895d2d8a2a51b992fd243e05
Cornelia Johnson <corneliaj91@gmail.com>
2020-10-22 15:08:01 -0400

    added toast for message delete

--- src/components/landlord/messages/MessageDelete.js
+++ src/components/landlord/messages/MessageDelete.js
@@ -3,14 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "reactstrap";
 import { MessageContext } from "./MessageProvider";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
 
 export const MessageDelete = (message) => {
   const { deleteMessage } = useContext(MessageContext);
+  const notify = () => {
+    toast("Basic Notication");
+  };
   return (
     <Button
       style={{ color: "darkcyan" }}
       color="link"
-      onClick={() => deleteMessage(message.id)}
+      onClick={() =>
+        deleteMessage(message.id).then(() => {
+          notify();
+        })
+      }
     >
       <FontAwesomeIcon icon={faTrash} />
     </Button>
